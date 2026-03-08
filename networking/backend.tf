2d36eb0df999a4a384630cddb9f659897a498dc5
learner <leraner@ow.com>
2026-03-08 06:31:54 +0000

    backend

--- networking/backend.tf
+++ networking/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "gcs" {
-    credentials = "/home/amit23comp/sa.json"
-    bucket  = "amitow23test123"
-    prefix  = "student.01-network-state"
+    credentials = "/home/altafpasha308/booming-landing-489605-j3-37042cde3484.json"
+    bucket  = "altafgcpbuckettest135"
+    prefix  = "networkingstate"
   }
 }
