82817d88805343b8f9b738edc861e9d2c26af6d0
learner <leraner@ow.com>
2026-03-08 07:31:23 +0000

    Added output for VPC and Subnet

---
+++ networking/output.tf
@@ -0,0 +1,21 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = google_compute_network.main-vpc.id
+}
+
+output "vpc_name" {
+  description = "The Name tag of the VPC"
+  # The "Name" tag value can be accessed via the tags attribute
+  value       = google_compute_network.main-vpc.id
+}
+
+output "subnet_ids" {
+  description = "List of IDs of all subnets"
+  value       = google_compute_subnetwork.subnet-1.id
+}
+
+output "subnet_names" {
+  description = "List of Names of all subnets"
+  value       = google_compute_subnetwork.subnet-1.name
+}
+
