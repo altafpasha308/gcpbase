3bee81ef2ef47bc322d7c53d1159ef830eaa2177
learner <leraner@ow.com>
2026-03-08 05:19:33 +0000

    Add

--- system/provider.tf
+++ system/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 }
 provider "google" {
-  credentials = file("/home/amit23comp/sa.json") # Path to your GCP credentials file
-  project     = "seraphic-lock-174915"           # Your GCP project ID
-  region      = "asia-south1-c"                  # Your desired region
+  credentials = file("/home/altafpasha308/booming-landing-489605-j3-37042cde3484.json") # Path to your GCP credentials file
+  project     = "booming-landing-489605-j3"           # Your GCP project ID
+  region      = "australia-southeast1"                  # Your desired region
 }
